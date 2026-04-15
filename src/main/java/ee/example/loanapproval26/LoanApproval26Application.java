2f47bdf29ca15dea70a9582a056fae26bed5f252
hsamelselg <hanna.samelselg@gmail.com>
2026-04-15 13:15:31 +0300

    initial commit

---
+++ src/main/java/ee/example/loanapproval26/LoanApproval26Application.java
@@ -0,0 +1,13 @@
+package ee.example.loanapproval26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApproval26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApproval26Application.class, args);
+    }
+
+}
---
+++ src/test/java/ee/example/loanapproval26/LoanApproval26ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.example.loanapproval26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApproval26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
